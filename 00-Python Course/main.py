f1d99e291c45ddb4fd3a4bde4a0743ec4bd8c3bd
Mohit Khangar <mohitkhangar.job@gmail.com>
2026-08-11 23:22:33 +0530

    Add files via upload

--- 00-Python Course/main.py
+++ 00-Python Course/main.py
@@ -1,2 +1,23 @@
-print("hey how are you \n I am good \\newline")
-print ( "hello \" world ")
+a =  34
+
+b = 2
+
+print("a+b=", a + b )
+print("a-b=", a - b )
+print("a/b=", a / b )
+print("a*b=", a * b )
+
+
+#conditional operators
+print(a>4)
+print(a==4)
+print(a==34)
+
+c = True
+d = False
+
+#logical operators
+print(c and c)
+print(c and d)
+print(d and c)
+print(d and d)
