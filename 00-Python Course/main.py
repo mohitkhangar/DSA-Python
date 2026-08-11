e5e76c514257553165076751763efa63f5e8d2ea
Mohit Khangar <mohitkhangar.job@gmail.com>
2026-08-11 23:15:36 +0530

    Add files via upload

--- 00-Python Course/main.py
+++ 00-Python Course/main.py
@@ -1,2 +1,12 @@
-print("hello world")
+age = 3
+print(age)
+print(type(age))
+
+cgpa = 8.43223
+print(cgpa)
+print(type(cgpa))
+
+name = "harry" 
+print(name)
+print(type(name))
 
---
+++ 00-Python Course/practice.py
@@ -0,0 +1,11 @@
+age = 3
+print(age)
+print(type(age))
+
+no = 8.24423
+print(no)
+print(type(no))
+
+name = "mohit"
+print(name)
+print(type(name))
