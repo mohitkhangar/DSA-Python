3fbf0141a9fc60f180f16fd410d3dd8f5bdfca7a
Mohit Khangar <mohitkhangar.job@gmail.com>
2026-08-21 22:00:47 +0530

    Add files via upload

---
+++ 00-Python Course/write copy.py
@@ -0,0 +1,9 @@
+f = open("john doe.txt", "a")
+
+string  = '''
+he initially lived in phoenix , arizona.He is a very nice guy
+
+'''
+f.write(string)
+
+f.close()
---
+++ 00-Python Course/write.py
@@ -0,0 +1,10 @@
+f = open("john doe.txt", "w")
+
+string  = '''
+john doe is nice guy. he lives in nyc and he works with python 
+His favourite package is pandas
+
+'''
+f.write(string)
+
+f.close()
