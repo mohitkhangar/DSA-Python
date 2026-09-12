8f532658e1c7689c97590dbd19685318e12e76b5
Mohit Khangar <mohitkhangar.job@gmail.com>
2026-09-12 23:41:17 +0530

    Add files via upload

---
+++ 00-Python Course/TIME COMPLEXITY/problem_10.py
@@ -0,0 +1,6 @@
+def print_elements_twice(arr):
+    for item in arr:
+        print(item)
+
+    for item in arr:
+        item(arr)
---
+++ 00-Python Course/TIME COMPLEXITY/problem_11.py
@@ -0,0 +1,5 @@
+def fixed_inner_loop(n):
+    for i in range(n):
+        for j in range(100):
+            print(i + j)
+
